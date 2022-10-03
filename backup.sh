c81197abcd0aa36046821a84a49540104739e9ab
vibakar <vibakar23@gmail.com>
2022-10-03 17:15:14 +0100

    Updated script after testing

--- backup.sh
+++ backup.sh
@@ -2,6 +2,8 @@
 
 jenkins_version="${1}"
 now=$(date +'%y%m%d%H%M%S')
+jenkins_home="/var/lib/jenkins"
+jenkins_war_path="/usr/share/java"
 
 # Check for jenkins version in the argument
 if [ "$#" -ne 1 ]; then
@@ -10,16 +12,23 @@ if [ "$#" -ne 1 ]; then
     exit 1
 fi
 
-# Create directories
-mkdir -p /tmp/jenkins/${now}/{backup,war}
+echo "Create directories"
+mkdir -p /tmp/jenkins/${now}/backup
+mkdir -p /tmp/jenkins/${now}/war
+mkdir -p /usr/lib/jenkins
 
-# Download latest jenkins war file
-curl https://get.jenkins.io/war-stable/${jenkins_version}/jenkins.war -o /tmp/jenkins/jenkins.war
+echo "Downloading Jenkins War file"
+curl -L https://get.jenkins.io/war-stable/${jenkins_version}/jenkins.war -o /tmp/jenkins/jenkins.war
 
 # Backup jenkins and replace war file
-tar -zcf /tmp/jenkins/${now}/backup/jenkins.tgz /var/lib/jenkins
-cp /usr/lib/jenkins/jenkins.war /tmp/jenkins/backup/${now}/war
-cp /tmp/jenkins/jenkins.war /usr/lib/jenkins/
+echo "Taking the backup of jenkins plugins, conf etc.."
+tar -zcf /tmp/jenkins/${now}/backup/jenkins.tgz -P ${jenkins_home}
 
-# Restart Jenkins
-systemctl restart jenkins
+echo "Taking the backup of jenkins war file"
+cp ${jenkins_war_path}/jenkins.war /tmp/jenkins/${now}/war
+
+echo "Replacing the old war file with latest"
+sudo cp /tmp/jenkins/jenkins.war ${jenkins_war_path}/jenkins.war
+
+echo "Restarting Jenkins"
+sudo systemctl restart jenkins
